ec248a7e6f9bb8dbf6ed10e0c9431ca36293cbfc
AlanTas <alantas@outlook.com>
2020-09-05 19:48:49 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import "dart:io";
+import "package:flutter/material.dart";
+import "package:path_provider/path_provider.dart";
+import "appointments/Appointments.dart";
+import "contacts/Contacts.dart";
+import "notes/Notes.dart";
+import "tasks/Tasks.dart";
+import "utils.dart" as utils;
+void main() {
+  startMeUp() async {
+    Directory docsDir =
+    await getApplicationDocumentsDirectory();
+    utils.docsDir = docsDir;
+    runApp(FlutterBook());
+  }
+  startMeUp();
+}
